4e05a33617a7a1380a6727ae7eca9a299681c08b
K_shivasali <66362181+kotreshsb16@users.noreply.github.com>
2020-07-17 15:46:23 +0530

    Update nag1.sh

--- nag1.sh
+++ nag1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 #echo "hi world...." | sudo tee /home/ubuntu/kotresha/jai
 #sudo service nagios status
-ssh ubuntu@172.31.22.31 'sudo service nagios stop'
+ssh ubuntu@172.31.22.31 'sudo service nagios start'
 
