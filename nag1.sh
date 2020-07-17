01443bcdb483ea1b63c1261587e366c3d9c23f34
K_shivasali <66362181+kotreshsb16@users.noreply.github.com>
2020-07-17 15:59:13 +0530

    Update nag1.sh

--- nag1.sh
+++ nag1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 #echo "hi world...." | sudo tee /home/ubuntu/kotresha/jai
 #sudo service nagios status
-ssh ubuntu@172.31.22.31 "'cd hiyan' 'echo "While I thought that I was learning how to live, I have been learning how to die." > quote1.txt'"
+ssh ubuntu@172.31.22.31 "'sudo cd hiyan' 'echo "While I thought that I was learning how to live, I have been learning how to die." > quote1.txt'"
 '
 
