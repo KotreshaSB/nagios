6b38fb9331c6b6d886bcac36acd2209c0c8d8bc9
K_shivasali <66362181+kotreshsb16@users.noreply.github.com>
2020-07-17 16:29:19 +0530

    Update nag1.sh

--- nag1.sh
+++ nag1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 #echo "hi world...." | sudo tee /home/ubuntu/kotresha/jai
 #sudo service nagios status
-ssh ubuntu@172.31.22.31 "'cd /home/ubuntu/hiyan'
-'sudo touch sales.txt'"
+ssh ubuntu@172.31.22.31 'cd /home/ubuntu/hiyan'
+#'sudo touch sales.txt'"
 
 
