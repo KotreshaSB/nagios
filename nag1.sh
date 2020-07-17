64a7f73ca84e94aa2b868870fc923f683da0c68a
K_shivasali <66362181+kotreshsb16@users.noreply.github.com>
2020-07-17 18:29:14 +0530

    Update nag1.sh

--- nag1.sh
+++ nag1.sh
@@ -1,7 +1,14 @@
 #!/bin/sh 
 #echo "hi world...." | sudo tee /home/ubuntu/kotresha/jai
 #sudo service nagios status
-ssh ubuntu@172.31.22.31 'sudo vi /etc/apache2/conf-available/nagios.conf'
+ssh ubuntu@172.31.22.31 
+'addr=$1
+port=$2
+user=$3
+sed -i -e "s/\(address=\).*/\1$1/" \
+-e "s/\(port=\).*/\1$2/" \
+-e "s/\(username=\).*/\1$3/" xyz.cfg'
+#'sudo vi /etc/apache2/conf-available/nagios.conf'
 
 #'cd /home/ubuntu/hiyan'
 #'sudo touch sales.txt'"
