699b75ebcc95cc735ed782b0422c898c2292a65a
K_shivasali <66362181+kotreshsb16@users.noreply.github.com>
2020-07-17 14:40:19 +0530

    Update nag1.sh

--- nag1.sh
+++ nag1.sh
@@ -1,7 +1,3 @@
 #!/bin/sh 
 ssh ubuntu@172.31.22.31
-service nagios status | grep 'active (running)' > /dev/null 2>&1
-if [ $? != 0 ]
-then
-        sudo service nagios stop > /dev/null
-fi
+echo "hi world" | sudo tee /home/ubuntu/kotresha/jai
